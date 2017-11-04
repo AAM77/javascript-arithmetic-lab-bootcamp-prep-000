006a25045d1cbcb46f7042970a7ace2142a45327
AAM77 <adeel.a.mhd@gmail.com>
2017-11-04 20:13:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function multiply(a, b) {
 function divide(a, b) {
   return (a / b);
 }
+
+function inc(n) {
+  n = n + 1;
+  return (n);
+}
+
+function dec(n) {
+  n = n - 1;
+  return (n);
+}
+
+
